1c8735d9357c89aa7c4bace61d9f23f081ba2c65
alexber1999 <alexber1999@gmail.com>
2018-07-29 02:06:16 -0400

    Constructor, Sum, and activation function
    Wrote the constructor for perceptron

--- perceptron.cpp
+++ perceptron.cpp
@@ -1,1 +1,10 @@
 #include "perceptron.h"
+
+
+Perceptron::Perceptron(int numWeights){
+    inputs = new double[numWeights];
+    weights = new double[numWeights];
+    for(int i = 0; i < numWeights; i++){
+        weights[i] = 0;
+    }
+}
--- perceptron.h
+++ perceptron.h
@@ -2,6 +2,17 @@
 #define Perceptron
 
 class Perceptron{
+    private:
+        double* inputs;
+        double* weights;
+    public:
+    
+    Perceptron(int numInputs);
+    //step 1 is the sum of inputs times weights
+    double sum(double*, double*);
+    //step 2 is the activation function(conform the output to some desired range)
+    int activate(double);
+    
     
 };
 #endif

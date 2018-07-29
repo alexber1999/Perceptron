321b60e303d09aa0d23727e435a49cac70cbbb1c
alexber1999 <alexber1999@gmail.com>
2018-07-29 02:16:23 -0400

    Added destructor, activate and guess functions

--- perceptron.cpp
+++ perceptron.cpp
@@ -1,10 +1,35 @@
 #include "perceptron.h"
 
 
-Perceptron::Perceptron(int numWeights){
+Perceptron::Perceptron(int numInputs){
     inputs = new double[numWeights];
     weights = new double[numWeights];
+    this -> numInputs = numInputs;
     for(int i = 0; i < numWeights; i++){
         weights[i] = 0;
     }
 }
+
+Perceptron::~Perceptron(){
+    delete [] inputs;
+    delete [] weights;
+}
+
+int Perceptron::activate(double sum){
+    if(sum > 0){
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+int Perceptron::guess(double* inputs, double* weights){
+    double sum = 0;
+    for(int i = 0; i < numInputs; i++){
+        sum += inputs[i] * weights[i];
+    }
+    
+    return activate(sum);
+}
+
+
--- perceptron.h
+++ perceptron.h
@@ -5,14 +5,15 @@ class Perceptron{
     private:
         double* inputs;
         double* weights;
+        int numInputs;
     public:
     
-    Perceptron(int numInputs);
+    Perceptron(int);
+    ~Perceptron();
     //step 1 is the sum of inputs times weights
-    double sum(double*, double*);
+    int guess(double*, double*);
     //step 2 is the activation function(conform the output to some desired range)
     int activate(double);
-    
     
 };
 #endif
